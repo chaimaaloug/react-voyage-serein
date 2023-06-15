8c257e27997d91c3fb7194d6138b687261318c9e
Pierre Beuselinck <pierrebeuselinck2@gmail.com>
2023-06-15 16:01:05 +0200

    add app.scss to app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import { getPlacesData } from './api/api';
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact'
 import './App.scss';
-import Button from './components/Button/Button'
-import TabsBar from './components/TabsBar/TabsBar'
-import Dropdown from './components/Dropdowns/Dropdown'
-import Tab from './components/Tab/Tab'
-import { Switch } from '@mui/material/Switch';
 
-
-const App = () => {
-
-    const [places, setPlaces] = useState([])
-    //get data from TravelAdvisor api
-    useEffect(() => {
-        getPlacesData()
-            .then((data) => {
-                setPlaces(data)
-            });
-    }, []);
-
-    return (
-        <div>
-           
-            <h1>Voyage Serein</h1>
-            <Switch defaultChecked />
-            <Button text="bouton" />
-            <Button text="bouton" outline />
-            <Dropdown></Dropdown>
-            <TabsBar>
-                <Tab text="Potato"/>
-                <Tab text="Potato" active/>
-                <Tab text="Potato"/>
-            </TabsBar>
-        </div>
-    );
-}
-
-export default App;
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  )}
