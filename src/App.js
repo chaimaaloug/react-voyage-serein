01db49a0c568bc9e0b9a2b67472867a00635a994
CHAIMAA LOUGSARI <49941834+chaimaaloug@users.noreply.github.com>
2023-06-15 14:57:30 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import { getPlacesData } from './api/api';
-import './App.scss';
-import Button from './components/Button/Button'
-import TabsBar from './components/TabsBar/TabsBar'
-import Dropdown from './components/Dropdowns/Dropdows'
-import Tab from './components/Tab/Tab'
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact'
 
-
-const App = () => {
-
-    const [places, setPlaces] = useState([])
-    //get data from TravelAdvisor api
-    useEffect(() => {
-        getPlacesData()
-            .then((data) => {
-                setPlaces(data)
-            });
-    }, []);
-
-    return (
-        <div>
-           
-            <h1>Voyage Serein</h1>
-            <Button text="bouton" />
-            <Button text="bouton" outline />
-            <Dropdown></Dropdown>
-            <TabsBar>
-                <Tab text="Potato"/>
-                <Tab text="Potato" active/>
-                <Tab text="Potato"/>
-            </TabsBar>
-        </div>
-    );
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+import Button from '../components/Button/Button'
+import { getPlacesData } from '../api/api';
+import TabsBar from '../components/TabsBar/TabsBar'
+import Tab from '../components/Tab/Tab'
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+
+    const [places, setPlaces] = useState([])
+    //get data from TravelAdvisor api
+    useEffect(() => {
+        getPlacesData()
+            .then((data) => {
+                setPlaces(data)
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Voyage Serein - Home Page</h1>
+            <Button text="bouton" />
+            <Button text="bouton" outline />
+            <TabsBar>
+                <Tab text="Potato"/>
+                <Tab text="Potato" active/>
+                <Tab text="Potato"/>
+            </TabsBar>
+
+            <Link to="/contact">Link to contact page</Link> <br />
+            <Link to="/about">Link to contact about</Link>
+            <p>Css Testing</p>
+        </div>
+    );
+
+} 
+
+
+export default Home;
