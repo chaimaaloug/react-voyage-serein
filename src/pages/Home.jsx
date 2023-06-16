9df184a08a4ea0ec897e9191662c21156f01fcb0
Pierre Beuselinck <pierrebeuselinck2@gmail.com>
2023-06-16 16:38:40 +0200

    maj page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,14 +21,16 @@ const Home = () => {
         <div>
             <BackgroundVideo url="https://www.youtube.com/embed/CLk7A7HXhYQ" />
             <div className='u-container'>
-                <p className="u-font-80 u-font-600">Fini la prise de tête<br />pour choisir où partir.</p>
-                <p className="u-font-32 u-mt-32">Découvrez ou vous partez cet été en 4 étapes.</p>
-                <div className='u-mt-32 u-flex u-flex-gap-24'>
-                    <Button text="Laisser le hasard choisir à ma place" outline />
-                    <Button text="Au hasard" />
+                <div className='l-home__content'>
+                    <p className="u-font-80 u-font-32@md u-font-600 u-color--bleu-marine">Fini la prise de tête<br />pour choisir où partir.</p>
+                    <p className="u-font-32 u-font-600 u-color--bleu-marine u-mt-32">Découvrez ou vous partez cet été en 4 étapes.</p>
+                    <div className='u-mt-32 u-flex u-flex-gap-24'>
+                        <Button text="Laisser le hasard choisir à ma place" outline />
+                        <Button text="Au hasard" />
+                    </div>
+                    {/* <Link to="/contact">Link to contact page</Link> <br />
+                <Link to="/about">Link to contact about</Link> */}
                 </div>
-                {/* <Link to="/contact">Link to contact page</Link> <br />
-            <Link to="/about">Link to contact about</Link> */}
             </div>
         </div>
     );
