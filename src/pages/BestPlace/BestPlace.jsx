be6b2c409635f34231b89053e04fbb7830cac163
CHAIMAA LOUGSARI <49941834+chaimaaloug@users.noreply.github.com>
2023-06-19 15:44:31 +0200

    updating images

--- src/pages/BestPlace/BestPlace.jsx
+++ src/pages/BestPlace/BestPlace.jsx
@@ -1,24 +1,23 @@
 import Title from "../../components/Title/Title";
 import Button from "../../components/Button/Button"
 import CardList from "../../components/Cards/Card";
-import style from "./BestPlace.module.scss"
  
-const BestPlaceCard = (props) => {
+const BestPlaceCard = () => {
 
     const data = [ 
         {
             id: 1,
-            image: require('../../assets/BestPlaces/3.png'),
+            image: require('../../assets/BestPlaces/3.jpg'),
             title: 'Les plages aux eaux turquoises et le clapotis des vagues',
         },
         {
             id: 2,
-            image: require('../../assets/BestPlaces/1.png'),
+            image: require('../../assets/BestPlaces/1.jpg'),
             title: 'Le dynamisme de la ville et ses mille et une facettes',
         },
         {
             id: 3,
-            image: require('../../assets/BestPlaces/2.png'),
+            image: require('../../assets/BestPlaces/2.jpg'),
             title: 'Les couleurs verdoyantes et la brise des montagnes',
         },
     ];
