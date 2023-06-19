d3d49731c8e90a08810b6339cb2e0eaca5e223e0
bore-prog <philippeaurore@orange.fr>
2023-06-19 16:02:19 +0200

    FEAT Badge with all styles

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -19,12 +19,12 @@ const Badge = (props) => {
         <div className={classes} onClick={callBack} >
             <label for={text}>
                 <input type="checkbox" id={text} ></input>
-                    {/* <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <span>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M19 5L5 19M19 19L5 5" stroke-width="2" stroke-linecap="round"/>
                     </svg>
-
-                    <img className={style.badgeImg} src={image} alt="" stroke="blue" /> */}
-                    <span>{text}</span>
+                    {text}
+                </span>
             </label>
         </div>
     )
