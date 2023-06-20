057b9b698337f6e63e85793844614e4494f95b33
bore-prog <philippeaurore@orange.fr>
2023-06-20 16:57:33 +0200

    UPDATE font title of home page to KoHo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
             <div className='u-container'>
                 <Header />
                 <div className='l-home__content'>
-                    <p className="u-font-80 u-font-32@md u-font-600 u-color--bleu-marine">Fini la prise de tête<br />pour choisir où partir.</p>
+                    <p className="u-font-80 u-font-32@md u-font-600 u-color--bleu-marine u-font-secondary">Fini la prise de tête<br />pour choisir où partir.</p>
                     <p className="u-font-32 u-font-16@md u-font-600 u-color--bleu-marine u-mt-32">Découvrez ou vous partez cet été en 4 étapes.</p>
                     <div className='u-mt-32 u-flex u-flex-wrap u-flex-gap-24'>
                         <Button text="Laisser le hasard choisir à ma place" onClick={navigateToOnboardingHasard} outline />
