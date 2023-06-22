3eb72d5b47ad2eaf0064f7c67015db664b55a6a9
bore-prog <philippeaurore@orange.fr>
2023-06-22 16:28:56 +0200

    UPDATE Loading.js with assets and styles

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import Button from "../../components/Button/Button";
 import Title from "../../components/Title/Title";
 import {useNavigate} from 'react-router-dom';
-import chargement from "../../assets/Frame 1527.png";
-import videoCharg from "../../assets/Morphing_1.mp4";
-import LottieControl from "./LottieControl";
+import videoCharg from "../../assets/Morphing_2.mp4";
+import style from "./Loading.module.scss";
 
 
 const Distance = () => {
@@ -20,22 +19,18 @@ const Distance = () => {
         <>
             <div className="l-steps">
                 <div className="l-steps__progressBar">
-                    <div className="l-steps__progressBarFill Fill90" ></div>
+                    <div className="l-steps__progressBarFill Fill100" ></div>
                 </div>
                 <div className="l-steps__text">
-                    <img src={chargement} alt="" />
-                    {/* <script type= "module" src="https://unpkg.com/@dotlottie/player-component@1.0.0/dist/dotlottie-player.js"></script>
-                    <dotlottie-player autoplay loop mode="normal" src="https://static9.lottiefiles.com/lotties/01_ramen_character.lottie" style={{ width: "320px", height:"320px" }}></dotlottie-player> */}
-                    <LottieControl />
-
-                    <video src={videoCharg} autoPlay="true"></video>
+                    <video autoPlay muted playsInline loop width={"200px"}>
+                        <source src={videoCharg} type="video/mp4" />
+                    </video>
                     <p className="l-steps__step">4/4</p>
                     <Title title="Vos réponses m'ont inspiré"/>
                     <p className="l-steps__description">Encore quelques instants avant de découvrir vos itinéraires de rêves.</p>
-                    <Button
-                        text="C'est presque prêt"
-                        onClick = {navigateToHome}
-                    />
+                    
+                    <button type="button" className="l-steps__button" disabled>C'est presque prêt</button>
+                    <button type="button" className="l-steps__button" onClick={navigateToHome}>C'est prêt !</button>
                 </div>
             </div>
         </>
