4e8fdd7d8333734f22dea28f6107f403507f5b13
Pierre Beuselinck <pierrebeuselinck2@gmail.com>
2023-06-22 13:20:18 +0200

    soft rest git - bug uploading file to large

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -3,6 +3,7 @@ import Button from "../../components/Button/Button";
 import Title from "../../components/Title/Title";
 import {useNavigate} from 'react-router-dom';
 import chargement from "../../assets/Frame 1527.png";
+import videoCharg from "../../assets/Morphing_1.mp4";
 import LottieControl from "./LottieControl";
 
 
@@ -26,6 +27,8 @@ const Distance = () => {
                     {/* <script type= "module" src="https://unpkg.com/@dotlottie/player-component@1.0.0/dist/dotlottie-player.js"></script>
                     <dotlottie-player autoplay loop mode="normal" src="https://static9.lottiefiles.com/lotties/01_ramen_character.lottie" style={{ width: "320px", height:"320px" }}></dotlottie-player> */}
                     <LottieControl />
+
+                    <video src={videoCharg} autoPlay="true"></video>
                     <p className="l-steps__step">4/4</p>
                     <Title title="Vos réponses m'ont inspiré"/>
                     <p className="l-steps__description">Encore quelques instants avant de découvrir vos itinéraires de rêves.</p>
