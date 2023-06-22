1160688424fa37c868ac3775d260d1d6b132c41c
Pierre Beuselinck <pierrebeuselinck2@gmail.com>
2023-06-22 15:18:37 +0200

    update destination + add map

---
+++ src/components/DressingCard/DressingCard.jsx
@@ -0,0 +1,30 @@
+import style from "./DressingCard.module.scss"
+
+
+const DressingCard = ({ data }) => {
+    return (
+        <div className={style.DressingCard}>
+            {data.map((item, index) => (
+                <DressingCardItem key={index} icon={item.icon} text={item.text} count={item.count} />
+            ))}
+        </div>
+    );
+};
+
+const DressingCardItem = (props) => {
+    const { icon, text, count } = props
+
+    return (
+        <div className={style.DressingCardItem}>
+            <img src={icon} alt="" srcset="" width={34} height={34} />
+            <p className="u-flex-grow">
+                {text}
+            </p>
+            <span className="u-flex-shrink-none">
+                {count}
+            </span>
+        </div>
+    );
+}
+
+export default DressingCard;
